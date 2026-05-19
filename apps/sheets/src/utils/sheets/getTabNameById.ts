1c9f0322ad892a69d2cfcee23110630b3075397c
kkazzann <kamil.kazaniecki@beliani.net>
2026-05-19 14:11:16 +0200

    refactor: delete files in favor of OOP restructurization

--- apps/sheets/src/utils/sheets/getTabNameById.ts
+++
@@ -1,8 +0,0 @@
-import { getDynamicTranslations } from '../../googleAuth'
-
-export async function getTabNameById(id: number, year?: string) {
-  const document = await getDynamicTranslations(year)
-  const sheet = document?.sheetsById?.[id]
-
-  return sheet?.title
-}

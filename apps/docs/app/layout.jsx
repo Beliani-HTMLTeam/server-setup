0a795e8a9702ca89ef4ffc6198269ad613f38afd
kkazzann <kamil.kazaniecki@beliani.net>
2026-05-26 09:42:46 +0200

    feat: update metadata for / page

--- apps/docs/app/layout.jsx
+++ apps/docs/app/layout.jsx
@@ -3,8 +3,8 @@ import 'nextra-theme-docs/style.css'
 import '../styles/main.css'
 
 export const metadata = {
-    title: "dokjumentas",
-    description: "documentation"
+    title: "Auth Wall - HTML Team Docs",
+    description: "Documentation for all HTML Team projects.",
 }
 
 export default function RootLayout({ children }) {

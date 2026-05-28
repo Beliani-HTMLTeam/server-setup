5239e6e8b8571cb9cd8de9e22b19e640e3d47396
kkazzann <kamil.kazaniecki@beliani.net>
2026-05-28 15:19:30 +0200

    refactor: better metadata extraction

--- apps/docs/app/docs/[[...mdxPath]]/page.jsx
+++ apps/docs/app/docs/[[...mdxPath]]/page.jsx
@@ -4,12 +4,15 @@ import { useMDXComponents as getMDXComponents } from '../../../mdx-components'
 export const generateStaticParams = generateStaticParamsFor('mdxPath')
 
 export async function generateMetadata(props) {
-  const params = await props.params
-  const { metadata } = await importPage(params.mdxPath)
-  return metadata
+  const { mdxPath } = await props.params
+  const { metadata } = await importPage(mdxPath)
+  return {
+    title: metadata.title || 'Nextra',
+    description: metadata.description || 'The next site builder',
+  }
 }
 
-export const dynamic = 'force-static';
+export const dynamic = 'force-static'
 
 const Wrapper = getMDXComponents().wrapper
 

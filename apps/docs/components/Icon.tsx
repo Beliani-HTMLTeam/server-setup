39d73e837f136ec59fc309182250c7f5ed9d7664
kkazzann <kamil.kazaniecki@beliani.net>
2026-05-28 15:18:02 +0200

    refactor: optimize icon component

--- apps/docs/components/Icon.tsx
+++ apps/docs/components/Icon.tsx
@@ -1,35 +1,54 @@
 'use client'
 
+import type { ComponentType } from 'react'
+
 import BunIcon from '@iconify-react/material-icon-theme/bun'
 import NextraIcon from '@iconify-react/simple-icons/nextra'
 import WxtIcon from '@iconify-react/material-icon-theme/wxt'
 import ElysiaDarkIcon from '@iconify-react/skill-icons/elysia-dark'
 import JavascriptIcon from '@iconify-react/skill-icons/javascript'
 import TypescriptIcon from '@iconify-react/skill-icons/typescript'
-import GithubIcon from '@iconify-react/mdi/github';
-import LogoutIcon from '@iconify-react/mdi/logout';
+import GithubIcon from '@iconify-react/mdi/github'
+import LogoutIcon from '@iconify-react/mdi/logout'
+import FigmaIcon from '@iconify-react/material-icon-theme/figma'
+import GoogledocsIcon from '@iconify-react/simple-icons/googledocs'
 
-// we have to make a custom icon wrapper as iconify uses <script> which is not supported in nextra mdx
+type IconComponent = ComponentType<any>
 
-export function Icon({ name }: { name: string }) {
-  switch (name) {
-    case 'bun':
-      return <BunIcon suppressHydrationWarning />
-    case 'nextra':
-      return <NextraIcon suppressHydrationWarning />
-    case 'wxt':
-      return <WxtIcon suppressHydrationWarning />
-    case 'elysiajs':
-      return <ElysiaDarkIcon suppressHydrationWarning />
-    case 'javascript':
-      return <JavascriptIcon suppressHydrationWarning />
-    case 'typescript':
-      return <TypescriptIcon suppressHydrationWarning />
-    case 'github':
-      return <GithubIcon suppressHydrationWarning />
-    case 'logout':
-      return <LogoutIcon suppressHydrationWarning />
-    default:
-      return null
-  }
+const icons = {
+  bun: BunIcon,
+  nextra: NextraIcon,
+  wxt: WxtIcon,
+  elysiajs: ElysiaDarkIcon,
+  javascript: JavascriptIcon,
+  typescript: TypescriptIcon,
+  github: GithubIcon,
+  logout: LogoutIcon,
+  figma: FigmaIcon,
+  gdocs: GoogledocsIcon,
+} satisfies Record<string, IconComponent>
+
+export type IconName = keyof typeof icons
+
+interface IconProps {
+  name: IconName
+  inline?: boolean
+  size?: string
+  className?: string
+}
+
+export function Icon({ name, size = '20', className }: IconProps) {
+  const Component = icons[name]
+
+  if (!Component) return null
+
+  return (
+    <Component
+      suppressHydrationWarning
+      width={size}
+      height={size}
+      className={className}
+      style={{ display: 'inline' }}
+    />
+  )
 }

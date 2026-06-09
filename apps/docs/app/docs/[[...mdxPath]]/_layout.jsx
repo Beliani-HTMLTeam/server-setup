829a47d5e37dee697b2cef269a82b78f3a2ba217
kkazzann <kamil.kazaniecki@beliani.net>
2026-06-09 15:40:51 +0200

    feat: add problem reporting url

--- apps/docs/app/docs/[[...mdxPath]]/_layout.jsx
+++ apps/docs/app/docs/[[...mdxPath]]/_layout.jsx
@@ -1,22 +1,46 @@
-"use client"
-import { Layout } from 'nextra-theme-docs'
-import { usePathname } from 'next/navigation';
+"use client";
+import { Layout } from "nextra-theme-docs";
+import { usePathname } from "next/navigation";
 
-const LayoutCSR = ({ banner, navbar, pagemap, footer, docsRepositoryBase, children }) => {
-    return <Layout
-        banner={banner}
-        navbar={navbar}
-        pageMap={pagemap}
-        docsRepositoryBase={docsRepositoryBase}
-        footer={footer}
-        editLink={
-            <a href={`${docsRepositoryBase}/content/edit/main${usePathname().replace("docs/", "")}.mdx`} target="_blank" rel="noopener noreferrer">
-                Edit this page
-            </a>
-        }
+const LayoutCSR = ({
+  banner,
+  navbar,
+  pagemap,
+  footer,
+  docsRepositoryBase,
+  children,
+}) => {
+  return (
+    <Layout
+      banner={banner}
+      navbar={navbar}
+      pageMap={pagemap}
+      docsRepositoryBase={docsRepositoryBase}
+      footer={footer}
+      editLink={
+        <a
+          href={`${docsRepositoryBase}/content/edit/main${usePathname().replace("docs/", "")}.mdx`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Edit this page
+        </a>
+      }
+      feedback={{
+        content: (
+          <a
+            href={`https://github.com/Beliani-HTMLTeam/server-setup/issues/new?title=<few words of the problem> in ${usePathname().replace("docs/", "")}&body=Describe the issue you found in ${usePathname().replace("docs/", "")} here.`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Report an issue
+          </a>
+        ),
+      }}
     >
-        {children}
-    </Layout >
-}
+      {children}
+    </Layout>
+  );
+};
 
-export default LayoutCSR
+export default LayoutCSR;

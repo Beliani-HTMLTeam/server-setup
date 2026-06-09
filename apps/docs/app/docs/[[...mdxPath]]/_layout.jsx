6e34204f636949a8e4ea0673b84106ed59f29064
kkazzann <kamil.kazaniecki@beliani.net>
2026-06-09 13:48:21 +0200

    fix: fix editLink

--- apps/docs/app/docs/[[...mdxPath]]/_layout.jsx
+++ apps/docs/app/docs/[[...mdxPath]]/_layout.jsx
@@ -1,5 +1,6 @@
 "use client"
 import { Layout } from 'nextra-theme-docs'
+import { usePathname } from 'next/navigation';
 
 const LayoutCSR = ({ banner, navbar, pagemap, footer, docsRepositoryBase, children }) => {
     return <Layout
@@ -8,6 +9,11 @@ const LayoutCSR = ({ banner, navbar, pagemap, footer, docsRepositoryBase, childr
         pageMap={pagemap}
         docsRepositoryBase={docsRepositoryBase}
         footer={footer}
+        editLink={
+            <a href={`${docsRepositoryBase}/content/edit/main${usePathname().replace("docs/", "")}.mdx`} target="_blank" rel="noopener noreferrer">
+                Edit this page
+            </a>
+        }
     >
         {children}
     </Layout >

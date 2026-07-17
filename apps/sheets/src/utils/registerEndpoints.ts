1aa1f6991ef8b4963955ebd94c7e87933320cb0b
kkazzann <kamil.kazaniecki@beliani.net>
2026-07-17 14:30:07 +0200

    chore: deprecate admin grouop

--- apps/sheets/src/utils/registerEndpoints.ts
+++ apps/sheets/src/utils/registerEndpoints.ts
@@ -1,7 +1,5 @@
-import { registerAdminGroup } from '../endpoints/admin.endpoint'
 import { registerMiscGroup } from '../endpoints/misc.endpoint'
 
 export function registerOther(app: any) {
-  registerAdminGroup(app)
   registerMiscGroup(app)
 }
